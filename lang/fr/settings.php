8add7fb21da6f8c6f0003f310fb29195a68c05db
Nicolas Friedli <nicolas@theologique.ch>
2013-12-16 21:15:23 +0100

    translation update

---
+++ lang/fr/settings.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['technique']             = 'Méthode d\'inclusion des polices';
+$lang['technique_o_fontface']  = '@font-face';
+$lang['technique_o_google']    = 'API Google Font';
+$lang['technique_o_kernest']   = 'Kernest';
+$lang['technique_o_cufon']     = 'Cufón';
+$lang['technique_o_typeface']  = 'typeface.js';
+$lang['technique_o_sifr']      = 'sIFR';
+$lang['fontFile']              = 'Nom de fichier de police (sans extension ; <strong>doit être uploadé en premier</strong>, voir les instructions)';
+$lang['fontName']              = 'Nom de police (tel qu\'utilisé dans la CSS ; pas nécessaire pour Cufón et sIFR)';
+$lang['headings']              = 'Quels entêtes doivent être modifiés ? (nécessaire à Cufón et sIFR ou si des styles doivent être ajoutés automatiquement)';
+$lang['addStyles']             = 'Ajouter des styles automatiquement ? (les styles pour Cufón et sIFR sont toujours ajoutés automatiquement, les autres peuvent être utilisés comme d\'habitude dans vos fichiers CSS)';
+$lang['genericFamily']         = 'Famille de polices par défaut';
