769b93b563527906a455a13e6cb73821749e79f1
Anika Henke <anika@selfthinker.org>
2015-04-05 14:27:54 +0100

    add option for second font (closes #3)

--- lang/en/settings.php
+++ lang/en/settings.php
@@ -3,10 +3,14 @@
  * English language file for config
  */
 
-$lang['technique']     = 'Font embedding technique';
-  $lang['technique_o_fontface'] = 'Uploaded font';
-  $lang['technique_o_google']   = 'Google Font';
-$lang['fontFile']      = 'Font file name (for uploaded fonts: without extension; for Google fonts: what comes after "?family=")';
-$lang['fontName']      = 'Font name (as used in CSS)';
-$lang['elements']      = 'For which elements should the font be used? (comma separated, not needed when font is used in other CSS files)';
-$lang['genericFamily'] = 'Generic font family';
+$lang['technique']      = 'Font embedding technique';
+  $lang['technique_o_fontface'] = 'Uploaded fonts';
+  $lang['technique_o_google']   = 'Google Fonts';
+$lang['fontFile']       = 'Font file name (for uploaded fonts: without extension; for Google fonts: what comes after "?family=")';
+$lang['fontName']       = 'Font name (as used in CSS)';
+$lang['elements']       = 'For which elements should the font be used? (comma separated, not needed when font is used in other CSS files)';
+$lang['genericFamily']  = 'Generic font family';
+$lang['fontFile2']      = 'Second font file name (for uploaded fonts: without extension; for Google fonts: what comes after "?family=")';
+$lang['fontName2']      = 'Second font name (as used in CSS)';
+$lang['elements2']      = 'For which elements should the second font be used? (comma separated, not needed when font is used in other CSS files)';
+$lang['genericFamily2'] = 'Second generic font family';
