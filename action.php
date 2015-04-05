6ab932f5906fdc837688d4a9b1ea1cbbcf071559
Anika Henke <anika@selfthinker.org>
2015-04-05 14:27:54 +0100

    add option for second font

--- action.php
+++ action.php
@@ -19,6 +19,11 @@ require_once(DOKU_PLUGIN.'action.php');
  */
 class action_plugin_fontface extends DokuWiki_Action_Plugin {
 
+    public function __construct() {
+        $this->fontSysDir = DOKU_INC.'lib/plugins/fontface/fonts/';
+        $this->fontDir = DOKU_BASE.'lib/plugins/fontface/fonts/';
+    }
+
     // register hook
     function register(&$controller) {
         $controller->register_hook('TPL_METAHEADER_OUTPUT','BEFORE', $this, '_addFontCode');
@@ -32,18 +37,15 @@ class action_plugin_fontface extends DokuWiki_Action_Plugin {
      */
     function _addFontCode(&$event, $param) {
 
-        $pluginSysDir = DOKU_INC.'lib/plugins/fontface/';
-        $pluginDir    = DOKU_BASE.'lib/plugins/fontface/';
-        $libDir       = $pluginDir.'lib/';
-        $fontSysDir   = $pluginSysDir.'fonts/';
-        $fontDir      = $pluginDir.'fonts/';
-
-        $technique    = $this->getConf('technique');
-        $fontFileName = $this->getConf('fontFile');
-        $fontName     = $this->getConf('fontName');
+        $technique     = $this->getConf('technique');
+        $fontFileName  = $this->getConf('fontFile');
+        $fontFileName2 = $this->getConf('fontFile2');
+        $fontName      = $this->getConf('fontName');
+        $fontName2     = $this->getConf('fontName2');
         // config option 'elements' used to be called 'headings', fallback to old option for backwards-compatibility
-        $headings     = $this->getConf('headings');
-        $elements     = !empty($headings) ? $headings : $this->getConf('elements');
+        $headings      = $this->getConf('headings');
+        $elements      = !empty($headings) ? $headings : $this->getConf('elements');
+        $elements2     = $this->getConf('elements2');
 
         $CSSfiles = array();
         $CSSembed = '';
@@ -53,61 +55,31 @@ class action_plugin_fontface extends DokuWiki_Action_Plugin {
             return false;
         }
 
+        // incomplete sanity check
+        if (empty($fontName) && empty($fontFileName) && empty($fontName2) && empty($fontFileName2)) {
+            msg("The FontFace plugin is missing some config settings.", -1);
+            return false;
+        }
+
         // prepare CSS and JS to embed depending on the technique
         switch ($technique) {
             case 'fontface':
-                $fontEOT   = $fontFileName.'.eot';
-                $fontWOFF  = $fontFileName.'.woff';
-                $fontWOFF2 = $fontFileName.'.woff2';
-                $fontTTF   = $fontFileName.'.ttf';
-                $fontSVG   = $fontFileName.'.svg';
-
-                // check if at least ttf and woff files exist
-                if (!$this->_isFileOk($fontSysDir.$fontWOFF, $fontDir.$fontWOFF, 'fontFile') ||
-                    !$this->_isFileOk($fontSysDir.$fontTTF,  $fontDir.$fontTTF,  'fontFile')) {
-                    return false;
-                }
-
-                $CSSembed       = "@font-face {".NL.
-                                  "  font-family: '".$fontName."';".NL;
-                if (file_exists($fontSysDir.$fontEOT)) {
-                    $CSSembed   .= "  src: url('".$fontDir.$fontEOT."');".NL;
-                }
-                $CSSembed       .= "  src: ";
-                if (file_exists($fontSysDir.$fontEOT)) {
-                    $CSSembed   .= "       url('".$fontDir.$fontEOT."?#iefix') format('embedded-opentype'),".NL;
-                }
-                if (file_exists($fontSysDir.$fontWOFF2)) {
-                    $CSSembed   .= "       url('".$fontDir.$fontWOFF2."') format('woff2'),".NL;
-                }
-                $CSSembed       .= "       url('".$fontDir.$fontWOFF."') format('woff'),".NL;
-                $CSSembed       .= "       url('".$fontDir.$fontTTF."') format('truetype')";
-                if (file_exists($fontSysDir.$fontSVG)) {
-                    $CSSembed   .= ",".NL;
-                    $CSSembed   .= "       url('".$fontDir.$fontSVG."#".str_replace(' ', '', $fontName)."') format('svg')";
-                }
-                $CSSembed       .= ";".NL.
-                                   "  font-weight: normal;".NL.
-                                   "  font-style: normal;".NL.
-                                   "}";
+                $CSSembed .= $this->_getFontFaceCode($fontName, $fontFileName);
+                $CSSembed .= $this->_getFontFaceCode($fontName2, $fontFileName2);
                 break;
 
             case 'google':
-                // check if required option is set
-                if (empty($fontFileName)) {
-                    msg("The '<strong>fontFile</strong>' config setting is <strong>not set</strong>.", -1);
-                    return false;
-                }
-
-                $CSSfiles = array(
-                    'http://fonts.googleapis.com/css?family='.str_replace(' ', '+', $fontFileName)
-                );
+                $CSSfiles[] = $this->_getGoogleFontPath($fontFileName);
+                $CSSfiles[] = $this->_getGoogleFontPath($fontFileName2);
                 break;
         }
 
-        // add styles automatically if headings are set, otherwise set them through CSS as usual
-        if ( !empty($elements) ) {
-            $CSSembed .= $elements." { font-family: '".$fontName."', ".$this->getConf('genericFamily')."; }";
+        // add styles automatically if elements are given, otherwise set them through CSS as usual
+        if ( !empty($elements) && !empty($fontName) ) {
+            $CSSembed .= $elements." { font-family: '".$fontName."', ".$this->getConf('genericFamily')."; }".NL;
+        }
+        if ( !empty($elements2) && !empty($fontName2) ) {
+            $CSSembed .= $elements2." { font-family: '".$fontName2."', ".$this->getConf('genericFamily2')."; }".NL;
         }
 
         // include all relevant CSS files
@@ -138,6 +110,7 @@ class action_plugin_fontface extends DokuWiki_Action_Plugin {
      * @param string $file          File to check (path to system directory)
      * @param string $fileDisplay   File to display in error message (path to web directory)
      * @param string $fileConfig    Name of config option
+     * @return bool                 If file is okay
      */
     function _isFileOk($file, $fileDisplay, $fileConfig) {
         if (empty($file)) {
@@ -150,6 +123,70 @@ class action_plugin_fontface extends DokuWiki_Action_Plugin {
         return true;
     }
 
+    /**
+     * Get code to embed local uploaded font via @font-face
+     *
+     * @param string $fontName      Name of the font as used in CSS
+     * @param string $fontFileName  File name of the font without extension
+     * @return mixed                String of CSS to embed or Bool if there is nothing to embed
+     */
+    function _getFontFaceCode($fontName, $fontFileName) {
+        if (empty($fontName) || empty($fontFileName)) {
+            return false;
+        }
+
+        $fontSysDir = $this->fontSysDir;
+        $fontDir    = $this->fontDir;
+        $fontEOT    = $fontFileName.'.eot';
+        $fontWOFF   = $fontFileName.'.woff';
+        $fontWOFF2  = $fontFileName.'.woff2';
+        $fontTTF    = $fontFileName.'.ttf';
+        $fontSVG    = $fontFileName.'.svg';
+
+        // check if at least ttf and woff files exist
+        if (!$this->_isFileOk($fontSysDir.$fontWOFF, $fontDir.$fontWOFF, 'fontFile') ||
+            !$this->_isFileOk($fontSysDir.$fontTTF,  $fontDir.$fontTTF,  'fontFile')) {
+            return false;
+        }
+
+        $CSSembed       = "@font-face {".NL.
+                          "  font-family: '".$fontName."';".NL;
+        if (file_exists($fontSysDir.$fontEOT)) {
+            $CSSembed   .= "  src: url('".$fontDir.$fontEOT."');".NL;
+        }
+        $CSSembed       .= "  src: ";
+        if (file_exists($fontSysDir.$fontEOT)) {
+            $CSSembed   .= "       url('".$fontDir.$fontEOT."?#iefix') format('embedded-opentype'),".NL;
+        }
+        if (file_exists($fontSysDir.$fontWOFF2)) {
+            $CSSembed   .= "       url('".$fontDir.$fontWOFF2."') format('woff2'),".NL;
+        }
+        $CSSembed       .= "       url('".$fontDir.$fontWOFF."') format('woff'),".NL;
+        $CSSembed       .= "       url('".$fontDir.$fontTTF."') format('truetype')";
+        if (file_exists($fontSysDir.$fontSVG)) {
+            $CSSembed   .= ",".NL;
+            $CSSembed   .= "       url('".$fontDir.$fontSVG."#".str_replace(' ', '', $fontName)."') format('svg')";
+        }
+        $CSSembed       .= ";".NL.
+                           "  font-weight: normal;".NL.
+                           "  font-style: normal;".NL.
+                           "}".NL;
+        return $CSSembed;
+    }
+
+    /**
+     * Get path to Google font
+     *
+     * @param string $fontFileName  File name of the font without extension
+     * @return mixed                String of CSS file path to embed or Bool if there is nothing to embed
+     */
+    function _getGoogleFontPath($fontFileName) {
+        // check if required option is set
+        if (empty($fontFileName)) {
+            return false;
+        }
+        return 'http://fonts.googleapis.com/css?family='.str_replace(' ', '+', $fontFileName);
+    }
 
 }
 
--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -7,8 +7,12 @@
  */
 $lang['technique']             = 'Méthode d\'inclusion des polices';
 $lang['technique_o_fontface']  = '@font-face';
-$lang['technique_o_google']    = 'API Google Font';
+$lang['technique_o_google']    = 'Google Font';
 $lang['fontFile']              = 'Nom de fichier de police (sans extension ; <strong>doit être uploadé en premier</strong>, voir les instructions)';
 $lang['fontName']              = 'Nom de police (tel qu\'utilisé dans la CSS)';
 $lang['elements']              = 'Quels entêtes doivent être modifiés ? (si des styles doivent être ajoutés automatiquement)';
 $lang['genericFamily']         = 'Famille de polices par défaut';
+$lang['fontFile2']             = 'Nom de fichier de police (sans extension ; <strong>doit être uploadé en premier</strong>, voir les instructions)';
+$lang['fontName2']             = 'Nom de police (tel qu\'utilisé dans la CSS)';
+$lang['elements2']             = 'Quels entêtes doivent être modifiés ? (si des styles doivent être ajoutés automatiquement)';
+$lang['genericFamily2']        = 'Famille de polices par défaut';
